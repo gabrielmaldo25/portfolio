a47e59698138b8acf5ea5c75cfc0354eb255aefe
Gabriel <maldoangel@hotmail.com>
2023-04-25 12:21:18 -0400

    clean

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -43,42 +43,6 @@ const Sidebar = () => {
 
   return (
     <>
-      <div>
-        <nav className="navigation">
-          <a href="/" className="brand-name">
-            MacroSoft
-          </a>
-          <button
-            className="hamburger"
-            onClick={() => {
-              setIsNavExpanded(!isNavExpanded);
-            }}
-          >
-            {/* icon from heroicons.com */}
-            <FontAwesomeIcon
-              color="#4d4d4e"
-              icon={isNavExpanded ? faXmark : faBars}
-            />
-          </button>
-          <div
-            className={
-              isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-            }
-          >
-            <ul>
-              <li>
-                <a href="/home">Home</a>
-              </li>
-              <li>
-                <a href="/about">About</a>
-              </li>
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
       <div className="nav-bar">
         <Link className="logo" to="/">
           gm
