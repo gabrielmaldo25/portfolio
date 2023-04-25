b0ab646e4d505102a8134d2ed49349c9ad68731d
Gabriel <maldoangel@hotmail.com>
2023-04-25 12:16:27 -0400

    Improve about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,50 +21,42 @@ const About = () => {
             letterClass={"text-animate"}
           />
         </h1>
+
+        <p>
+          Hello, my name is Gabriel Maldonado and I'm a student of Computer
+          Science from the National University of Asunción in Paraguay.
+        </p>
         <p>
           I am a self-taught person, with the goals of constantly training
           myself, whether it be out of interest, due to work or student
-          challenges, and to grow in the area of computing.
-        </p>
-        <p align="LEFT">
-          Ability to relate, with a calm character and, above all, willing to
-          work as a team.
-        </p>
-        <p>Always looking to meet my goals.</p>
-        <p>
-          I'm open to new oportunities where I can prove myself and continue to
-          learn in the IT field.{" "}
+          challenges.
         </p>
         <p>
           At the moment my main focus is in the frontend development, I have a
           little more than a year of experience working with React and React
-          Native stacks as well as working with Agile Metodologies . But I'm
-          also open to learn new Frameworks with other languages such as Java
-          and Python.
+          Native stacks as well as working with Agile Methodologies.
+        </p>
+        <p>
+          I'm open to new oportunities where I can prove myself and continue
+          learning about those stacks to solidify my knowledge. But I'm also
+          open to learn new Frameworks with other languages such as Java and
+          Python.
+        </p>
+        <p>
+          I have the ability to relate, with a calm character and, above all,
+          willing to work as a team. Always looking to meet my goals.
+        </p>
+        <p>
+          My hobbies goes from watching some series and movies, hanging out with
+          my friends and family as much as I can.
+        </p>
+        <p>
+          I basically speak / understand three languages that includes Spanish,
+          Guaraní wich is the second official language of Paraguay and English,
+          that I'm always learning and trying new ways to improve it, as an
+          example is this whole portfolio.
         </p>
       </div>
-      {/* <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faJsSquare} color="#F0DB4F" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faReact} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faCss3} color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faPython} color="#EC4" />
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
